83f8264099fada783aa6537a7328d9fa8310e0cf
Philipe Almeida <devpalmeida@gmail.com>
2022-08-30 13:10:40 +0100

    automatic toggle theme by user SO preference

--- src/components/toggles/theme/ToggleTheme.tsx
+++ src/components/toggles/theme/ToggleTheme.tsx
@@ -9,11 +9,11 @@ const updateTheme = (isDarkEnabled: boolean) => {
 };
 
 export default function ToggleTheme() {
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isDark, setIsEnabled] = useState(false);
 
   useEffect(() => {
-    updateTheme(isEnabled);
-  }, [isEnabled]);
+    updateTheme(isDark);
+  }, [isDark]);
 
   const toggleState = () => {
     setIsEnabled((prevState) => !prevState);
@@ -24,13 +24,24 @@ export default function ToggleTheme() {
       toggleState();
   };
 
+  useEffect(() => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      if (!isDark)
+        toggleState();
+    });
+  
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', () => {});
+    }
+  });
+  
   return (
     <label
       className="toggle-wrapper"
       tabIndex={0} 
       onKeyDown={handleKeyDown} 
       htmlFor="toggle">
-      <div className={`toggle ${isEnabled ? "enabled" : "disabled"}`}>
+      <div className={`toggle ${isDark ? "enabled" : "disabled"}`}>
         <input
           id="toggle"
           name="toggle"
@@ -39,7 +50,7 @@ export default function ToggleTheme() {
         />
         <div className="toggle-selector">
           <div className="wrapper-icons">
-            {isEnabled ? <svg width="24" height="24" className="moon" viewBox="0 0 24 24" fill="#E9E9E9" xmlns="http://www.w3.org/2000/svg">
+            {isDark ? <svg width="24" height="24" className="moon" viewBox="0 0 24 24" fill="#E9E9E9" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 11.807C10.7418 10.5483 9.88488 8.94484 9.53762 7.1993C9.19037 5.45375 9.36832 3.64444 10.049 2C8.10826 2.38205 6.3256 3.33431 4.92899 4.735C1.02399 8.64 1.02399 14.972 4.92899 18.877C8.83499 22.783 15.166 22.782 19.072 18.877C20.4723 17.4805 21.4245 15.6983 21.807 13.758C20.1625 14.4385 18.3533 14.6164 16.6077 14.2692C14.8622 13.9219 13.2588 13.0651 12 11.807V11.807Z" />
             </svg> : <svg width="24" height="24" className="sun" viewBox="0 0 24 24" fill="#E9E9E9" xmlns="http://www.w3.org/2000/svg">
               <path d="M6.995 12C6.995 14.761 9.241 17.007 12.002 17.007C14.763 17.007 17.009 14.761 17.009 12C17.009 9.239 14.763 6.993 12.002 6.993C9.241 6.993 6.995 9.239 6.995 12ZM11 19H13V22H11V19ZM11 2H13V5H11V2ZM2 11H5V13H2V11ZM19 11H22V13H19V11Z" />
